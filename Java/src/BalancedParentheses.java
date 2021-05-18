e0e9c9a2e9e94e2df5d6212604131042e21313f1
Vamsi <vamsivinay119@gmail.com>
2021-05-17 22:56:35 -0400

    BalancedParentheses program using stack

---
+++ Java/src/BalancedParentheses.java
@@ -0,0 +1,86 @@
+/*
+Hi, here's your problem today. This problem was recently asked by Uber:
+
+Imagine you are building a compiler. Before running any code, the compiler must check that the parentheses in the program are balanced. Every opening bracket must have a corresponding closing bracket. We can approximate this using strings.
+
+Given a string containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+An input string is valid if:
+- Open brackets are closed by the same type of brackets.
+- Open brackets are closed in the correct order.
+- Note that an empty string is also considered valid.
+
+Example:
+Input: "((()))"
+Output: True
+
+Input: "[()]{}"
+Output: True
+
+Input: "({[)]"
+Output: False
+ */
+
+import java.util.*;
+
+public class BalancedParentheses {
+    // Method Checking Balanced Parentheses
+    static boolean areParenthesesBalanced(String expr)
+    {
+        // Using ArrayDeque is faster than using Stack class
+        Deque<Character> stack
+                = new ArrayDeque<Character>();
+
+        // Traversing the Expression
+        for (int i = 0; i < expr.length(); i++)
+        {
+            char x = expr.charAt(i);
+
+            if (x == '(' || x == '[' || x == '{')
+            {
+                // Push the element in the stack
+                stack.push(x);
+                continue;
+            }
+
+            // If current character is not opening
+            // bracket, then it must be closing. So stack
+            // cannot be empty at this point.
+            if (stack.isEmpty())
+                return false;
+            char check;
+            switch (x) {
+                case ')':
+                    check = stack.pop();
+                    if (check == '{' || check == '[')
+                        return false;
+                    break;
+
+                case '}':
+                    check = stack.pop();
+                    if (check == '(' || check == '[')
+                        return false;
+                    break;
+
+                case ']':
+                    check = stack.pop();
+                    if (check == '(' || check == '{')
+                        return false;
+                    break;
+            }
+        }
+
+        // Check Empty Stack
+        return (stack.isEmpty());
+    }
+
+    public static void main(String[] args)
+    {
+        String expr = "([{}])";
+
+        // Function call
+        if (areParenthesesBalanced(expr))
+            System.out.println("Balanced ");
+        else
+            System.out.println("Not Balanced ");
+    }
+}
