6be837cd8c2b6cf8e4b4017fcc21a9d5c83c3265
Vamsi <vamsivinay119@gmail.com>
2021-05-21 00:30:07 -0400

    Added node linking, traversal, still work needs to be done

--- Java/src/LinkedListProblem/LinkedListReverseAddition.java
+++ Java/src/LinkedListProblem/LinkedListReverseAddition.java
@@ -1,4 +1,6 @@
 package LinkedListProblem;
+import org.w3c.dom.Node;
+
 import java.util.LinkedList;
 import java.util.List;
 
@@ -15,13 +17,45 @@ Explanation: 342 + 465 = 807.
 */
 
 
-public class LinkedListReverseAddition{
+class LinkedListReverseAddition{
 
+    //head of the list
+    Node head;
+
+    //Linked List Node. This inner class is static so that main() can access it
+    static class Node{
+        int data;
+        Node next;
+        Node(int d){
+            data = d;
+            next = null;
+        }//constructor
+    }
+
+    public void printList(){
+        Node n = head;
+        while ( n !=null){
+            System.out.println(n.data+" ");
+            n = n.next;
+        }
+    }
+
+
+   //Method to create a SLL with three nodes
     public static void main(String[] args) {
 
         //Creating two LinkedLists
         LinkedList l1 = new LinkedList();
         LinkedList l2 = new LinkedList();
+
+        l1.head = new Node(1);
+        Node l1Second = new Node(2);
+        Node l1Third = new Node(3);
+
+        //Link first node to the second node
+        l1.head.next = l1Second;
+        //Link second node to the third node
+        l1Second.next = l1Third;
 
         //Adding integers to the linkedLists using add()
         l1.add(2);
@@ -34,13 +68,6 @@ public class LinkedListReverseAddition{
         //Printing out the list
         System.out.println("The list l1 has these elements: "+l1);
         System.out.println("The list l1 has these elements: "+l2);
-        
-        
 
-               
-
-        
-     
-    
     }
 }
